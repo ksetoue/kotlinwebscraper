4dec038ebf06bc2ee31f6de9ae9826780b45164c
ksetoue <karoline.kimiko@gmail.com>
2021-12-01 11:55:29 -0300

    :sparkles: add spring project

---
+++ src/main/kotlin/com/scraper/ibmapp/IbmappApplication.kt
@@ -0,0 +1,11 @@
+package com.scraper.ibmapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IbmappApplication
+
+fun main(args: Array<String>) {
+	runApplication<IbmappApplication>(*args)
+}
---
+++ src/test/kotlin/com/scraper/ibmapp/IbmappApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.scraper.ibmapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IbmappApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
