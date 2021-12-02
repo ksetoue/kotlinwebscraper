312d9aec801694c068f728eb0276a07cbf1c27ca
ksetoue <karoline.kimiko@gmail.com>
2021-12-01 22:37:10 -0300

    :wrench: work in progress

--- src/test/kotlin/com/scraper/ibmapp/application/LinkApplicationServiceTest.kt
+++ src/test/kotlin/com/scraper/ibmapp/application/LinkApplicationServiceTest.kt
@@ -3,10 +3,16 @@ package com.scraper.ibmapp.application
 import org.junit.jupiter.api.BeforeEach
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
 internal class LinkApplicationServiceTest {
     
     @BeforeEach
     fun setUp() {
+    }
+
+    @Test
+    fun test1() {
+
     }
 }
